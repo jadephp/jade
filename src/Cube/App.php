846b9f8dd17aa6c6d2a075a8631394628c05db48
slince <taosikai@yeah.net>
2019-08-05 14:00:37 +0800

    add

---
+++ src/Cube/App.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the Cube package.
+ *
+ * (c) Slince <taosikai@yeah.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Cube;
+
+use Jade\App as JadeApp;
+
+class App extends JadeApp
+{
+
+}
